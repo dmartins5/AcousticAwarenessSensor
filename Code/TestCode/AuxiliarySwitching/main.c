92e1b7d69876628b337adc4ab100187e642c843d
dmartins5 <43209946+dmartins5@users.noreply.github.com>
2019-11-29 18:27:06 -0500

    Update main.c

--- Code/TestCode/AuxiliarySwitching/main.c
+++ Code/TestCode/AuxiliarySwitching/main.c
@@ -14,23 +14,26 @@ int main(void)
 	sig = 0;
 	DDRB |= (1 << PORTB1)|(1 << PORTB0); // PORTB1:0 are outputs
 	
-	TCNT1 = 49999; // Sets the timer to go off every 1.004s.
+	TCNT1 = 39062; // Timer1 is invoked every second
 	TCCR1A = (0b00<<COM1A0)|(0b00<<COM1B0)|(0b00<<WGM10);
 	TCCR1B = (0b00<<WGM12)|(0b101<<CS10); // Set Prescaler to 1
 	TIMSK1 = (0b1<<TOIE1); // Enables interrupts for TOV1
 	
 	sei();
 	while (1) 
-    {
-		PORTB = sig;
-    }
+    	{
+		asm("NOP");
+	}
 }
 
 ISR (TIMER1_OVF_vect)
 {
 	cli();
  	sig ^= 3; // Toggles between PORTB1:0 both on and off
-	TCNT1 = 49999;
+	// By toggling the signal between 0 and 3, the auxiliary ports will be turned on and off
+	// Each side of the headphones is connected to PORTB1 and PORTB0
+	PORTB = sig;
+	TCNT1 = 39062;
 	sei();
 }
 
