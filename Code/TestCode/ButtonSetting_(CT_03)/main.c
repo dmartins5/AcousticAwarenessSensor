7ba977e35766001317d63a761fa0612613b38399
dmartins5 <43209946+dmartins5@users.noreply.github.com>
2020-03-03 17:02:41 -0500

    Update main.c

--- Code/TestCode/ButtonSetting_(CT_03)/main.c
+++ Code/TestCode/ButtonSetting_(CT_03)/main.c
@@ -1,12 +1,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+#include <avr/sleep.h>
 
 volatile uint8_t thresh_sensitivity __attribute__((address(0x800100)));
 // If thresh_sensitivity = 0, then Sensitivity is Low
 // If thresh_sensitivity = 1, then Sensitivity is Medium
 // If thresh_sensitivity = 2, then Sensitivity is High
-volatile uint8_t sleep_mode __attribute__((address(0x800101)));
+volatile uint8_t sleep_code __attribute__((address(0x800101)));
 // If sleep_mode = 0xFF, then the device is powered off
 // If sleep_mode = 0x00, then the device is powered on
 void WDT_init() __attribute__((naked)) __attribute__((section(".init3")));
@@ -23,69 +24,76 @@ int main()
 	DDRC &=~ 0x1F;
 	DDRD |= 0x07;
 	PORTD = 0x07;
+	sleep_code = 0x00;
 	thresh_sensitivity = 3;
 	PCICR = (1 << PCIE1);
+	PCMSK1 = (1 << PCINT12)|(1 << PCINT11)|(1 << PCINT10)|(1 << PCINT9)|(1 << PCINT8);
 
 	sei();
 	while(1)
 	{
-		//asm("NOP");
+		asm("NOP");
 	}
 }
 
 ISR (PCINT1_vect)
 {
-	cli(); // Disables all other Interrupts, making this an Atomic ISR
-	
+	cli();
 	if(PINC & (1 << PINC4))
 	{
-		if(sleep_mode == 0x00)
+		if(sleep_code == 0x00)
 		{
-			sleep_mode = 0xFF; // Entering Sleep Mode
-			SMCR = (1 << SM1);
-			SMCR |= (1 << SE);
-		} else {
-			SMCR &=~ (1 << SE); // Exiting Sleep Mode
-			sleep_mode = 0x00;
-			wdt_enable(WDTO_15MS);
+			sleep_code = 0xFF;
+		} else if (sleep_code == 0xFF) {
+			sleep_code = 0x00;
 		}
 	}
 	if(PINC & (1 << PINC3))
 	{
-		wdt_enable(WDTO_15MS);
+		wdt_enable(WDTO_250MS);
 		// Perform a Soft Reset on the System
 	}
 	if(PINC & (1 << PINC2))
-        {
+	{
 		thresh_sensitivity = 2;
 		// High Sensitivity On
-        }
-        if(PINC & (1 << PINC1))
-        {
+	}
+	if(PINC & (1 << PINC1))
+	{
 		thresh_sensitivity = 1;
 		// Medium Sensitivity On
-        }
+	}
 	if(PINC & (1 << PINC0))
-        {
+	{
 		thresh_sensitivity = 0;
 		// Low Sensitivity On
-        }
+	}
 	
 	switch(thresh_sensitivity)
 	{
 		case 0:
-			PORTD = 0x01;
-			break;
+		PORTD = 0x01;
+		break;
 		case 1:
-			PORTD = 0x02;
-			break;
+		PORTD = 0x02;
+		break;
 		case 2:
-			PORTD = 0x04;
-			break;
+		PORTD = 0x04;
+		break;
 		default:
-			PORTD = 0x07;
-			break;
+		PORTD = 0x07;
+		break;
 	}
 	
-	sei(); // Enable Interrupts before exiting the ISR
+	if(sleep_code == 0xFF)
+	{
+		sei();
+		PORTD = 0x00;
+		set_sleep_mode(2);
+		sleep_mode();
+	} else if(sleep_code == 0x00) {
+		sleep_disable();
+		wdt_enable(WDTO_250MS);
+	}
+	sei();
 }
